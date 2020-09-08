af5f7cc4f8acb1913517450cc8dcb571bfba4b5a
KJTempas <fw6927mm@go.minneapolis.edu>
2020-09-08 15:34:24 -0500

    add info to output

--- camelcase.py
+++ camelcase.py
@@ -1,9 +1,11 @@
 #git practice creating branches, merging; functions are separate branches
 def camel_case(sentence):
-    title_case = sentence.title() #uppercase first letter of each word
-    upper_camel_cased=title_case.replace(' ' ,'') #remove spaces
+    title_case = sentence.title() #uppercase first letter of each word - claraj version
+    upper_camel_cased=title_case.replace(' ' ,'') #remove spaces - claraj version
     #lowercase first letter, join w/ rest of string
     #slices don't produce index out of bounds errors
+
+    #first word in lower case and remaindert (index 1 to end) are title case
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
 def banner():
@@ -20,7 +22,7 @@ def main():
     instructions()
     sentence = input('Enter your sentence:  ')
     output = camel_case(sentence)
-    print(output)
+    print(f' Your sentence in camel case looks like this {output}')
 
 if __name__ == '__main__':
     main()
